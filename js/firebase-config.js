380481dbaf3bbc204f3a8c0641d3d46f28ce32c3
anajulialanca-eng <anajulia.lanca@gmail.com>
2026-07-25 14:34:11 -0300

    FullTank V5 - Login, garagem e melhorias

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCYlrC0iLEr6OmYgR15B4ieEjAAZzTAMKM",
+  authDomain: "fulltank-84db6.firebaseapp.com",
+  projectId: "fulltank-84db6",
+  storageBucket: "fulltank-84db6.firebasestorage.app",
+  messagingSenderId: "963753759132",
+  appId: "1:963753759132:web:b92d2621b0eadc8959b859",
+  measurementId: "G-DBEPXEFJ7J"
+};
