bee87943dcf6bb5c7c6d9660fefcf17b395169d7
anajulialanca-eng <anajulia.lanca@gmail.com>
2026-07-25 14:42:40 -0300

    FullTank V6 - lavagens, cuidados e melhorias

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='fulltank-v5-1';
+const CACHE='fulltank-v6-1';
 const FILES=['./','./index.html','./css/style.css','./js/app.js','./js/firebase-config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
